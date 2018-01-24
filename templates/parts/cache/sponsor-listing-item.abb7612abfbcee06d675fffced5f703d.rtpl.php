d223ede984b6a816a9c3d310432e5fe12a5aac03
Claudio Marangon <marangon.claudio@gmail.com>
2018-01-24 16:53:50 +0100

    Clean temp directories

--- templates/parts/cache/sponsor-listing-item.abb7612abfbcee06d675fffced5f703d.rtpl.php
+++
@@ -1,7 +0,0 @@
-<?php if(!class_exists('raintpl')){exit;}?><a class="listing-item reveal to-overlay" href="<?php echo $detail_url;?>">
-    <div class="image"><img src="<?php echo $image["src"];?>" alt="<?php echo $image["alt"];?>" /></div>
-    <div class="additional">
-        <h3><?php echo $title;?></h3>
-        <p class="discreet"><?php echo $description;?></p>
-    </div>
-</a>
